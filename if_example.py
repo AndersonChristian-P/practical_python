593a99e88662cdbce0a017a70f303117074ba026
Christian Anderson <ca96187@gmail.com>
2019-07-17 20:10:27 -0600

    reviewed flow control statements

---
+++ if_example.py
@@ -0,0 +1,5 @@
+name = "Alice"
+if name == "Alice":
+    print("Hi Alice") # block - lines of code that are indented at the same level - begins when the indentation increases and ends when the indentation decreases
+
+print("Done")
