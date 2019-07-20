2354788ce99c77254c9352a5854601e473a2ffd4
Christian Anderson <ca96187@gmail.com>
2019-07-19 20:38:03 -0600

    while loops

---
+++ Flow_Control/continue.py
@@ -0,0 +1,8 @@
+span = 0
+while span < 5:
+    span = span + 1
+    if span == 3:
+        continue
+    print('span is ' + str(span))
+
+# continue causes the while loop to jump back to the start of the while statement
---
+++ Flow_Control/yourName2.py
@@ -0,0 +1,10 @@
+name = ''
+while True:
+    print('Please type your name')
+    name = input()
+    if name == 'your name':
+        break
+print('Thank you')
+
+    
+# a break statement causes the execution to immediately leave the loop, without re-checking the condition
