7d2a1c1a6d2b04cd6be926d9e43751c15c36bcfd
Christian Anderson <ca96187@gmail.com>
2019-07-21 19:18:28 -0600

    added notes for For Loops

--- Flow_Control/Total.py
+++ Flow_Control/Total.py
@@ -2,3 +2,8 @@ total = 0
 for num in range(101):
     total = total + num
 print(total)
+
+
+# for loops will loop a specific number of times
+# the range() function called with one, two, or three arguments
+# break and continue can be used in for loops
