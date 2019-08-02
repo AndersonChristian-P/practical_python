fba92bf9b644892b5bf39af74b1929d09219a7a2
Christian Anderson <ca96187@gmail.com>
2019-08-02 16:45:26 -0600

    completed built-in functions

---
+++ Functions/sysexit.py
@@ -0,0 +1,11 @@
+import sys
+print('Hello')
+sys.exit()
+# once sys.exit is run it exits the application
+print('Goodbye')
+
+
+# you can import modules and get access to new functions
+# the modules that come with Python are called the standard library, but you can also install third-party modules using the pip tool
+# the sys.exit() function will immediately quit your program
+# the pyperclip third-party module has copy() and paste() functions for reading and writing text to the clipboard
