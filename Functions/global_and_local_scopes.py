4bcdf6b31a2cd64d96a1de62e2c28d2101f632f5
Christian Anderson <ca96187@gmail.com>
2019-08-15 12:06:34 -0600

    functions global and local scopes

---
+++ Functions/global_and_local_scopes.py
@@ -0,0 +1,8 @@
+# Global and Local Scope
+
+# a scope can be thought of as an area of the source, code, and as a container of variables
+# the global scope is code outside of all functions. variables assigned here are global vairables
+# each function's code is in its own local scope. variables assigned here are local variables
+# code in the global scope cannot use any local variables
+# code in a function's local scope cannout use variables in any other function's local scope
+# if there's an assignment statement for a vaiable in a function, that is a local variable
